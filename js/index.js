a938261620f57f84bab3a20c69d7f0acc94705c6
refine <jtragans@gmail.com>
2025-02-08 08:47:11 -0500

    time update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function formatElapsedTime() 
 {
   const currentTime = new Date();
-  const lastStreamEndTime = new Date("2025-02-06T04:49:59Z");
+  const lastStreamEndTime = new Date("2025-02-08T04:59:16Z");
   // in milliseconds
   const timeSinceStream = currentTime.getTime() - lastStreamEndTime.getTime();
   
