b4707612fd62adfcb677d517ba492a595a7b651f
Astro4545 <3903401+Astro4545@users.noreply.github.com>
2025-07-12 21:41:54 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function formatElapsedTime() 
 {
   const currentTime = new Date();
-  const lastStreamEndTime = new Date("2025-07-11T02:43:30Z");
+  const lastStreamEndTime = new Date("2025-07-13T01:41:30Z");
   // in milliseconds
   const timeSinceStream = currentTime.getTime() - lastStreamEndTime.getTime();
   
