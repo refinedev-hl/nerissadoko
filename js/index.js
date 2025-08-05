fd68a73ba83defd8a1fde8617393348d398a125e
Astro4545 <3903401+Astro4545@users.noreply.github.com>
2025-08-05 05:59:30 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function formatElapsedTime() 
 {
   const currentTime = new Date();
-  const lastStreamEndTime = new Date("2025-08-05T03:00:30Z");
+  const lastStreamEndTime = new Date("2025-08-05T09:55:30Z");
   // in milliseconds
   const timeSinceStream = currentTime.getTime() - lastStreamEndTime.getTime();
   
@@ -51,4 +51,5 @@ function buildRandomNerissaStreamLink()
 
 //builds the first link upon page load
 buildRandomNerissaStreamLink();*/
+
 
